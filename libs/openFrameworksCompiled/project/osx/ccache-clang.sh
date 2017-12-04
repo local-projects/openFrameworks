956e5228bc50e76d674731e288e6754c8a06647e
Oriol Ferrer Mesia <oriol@uri.cat>
2017-12-04 18:00:45 -0500

    basedir is home for ccace

--- libs/openFrameworksCompiled/project/osx/ccache-clang.sh
+++ libs/openFrameworksCompiled/project/osx/ccache-clang.sh
@@ -5,7 +5,7 @@ if type -p /usr/local/bin/ccache >/dev/null 2>&1; then
   export CCACHE_HARDLINK=true
   export CCACHE_NOHASHDIR=true
   export CCACHE_SLOPPINESS=file_macro,time_macros,include_file_mtime,include_file_ctime,file_stat_matches
-  export CCACHE_BASEDIR=~/
+  export CCACHE_BASEDIR=$HOME
   exec /usr/local/bin/ccache /usr/bin/clang "$@"
 else
   exec clang "$@"
