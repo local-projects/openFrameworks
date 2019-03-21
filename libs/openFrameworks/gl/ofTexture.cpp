9c13d33e3ee43eeed8393f436c3b9f05cb3a82e4
Oriol Ferrer Mesia <oriol@uri.cat>
2019-03-22 00:09:02 +0100

    add ofTexture compression capability

--- libs/openFrameworks/gl/ofTexture.cpp
+++ libs/openFrameworks/gl/ofTexture.cpp
@@ -456,6 +456,11 @@ void ofTexture::allocate(const ofTextureData & textureData, int glFormat, int pi
 #else
 	if(texData.textureTarget == GL_TEXTURE_2D){
 #endif
+
+        if (texData.compressionType != OF_COMPRESS_NONE) {
+            texData.glInternalFormat = GL_COMPRESSED_RGBA_S3TC_DXT3_EXT;
+        }
+
 		glBindTexture(texData.textureTarget,texData.textureID);
 		glTexImage2D(texData.textureTarget, 0, texData.glInternalFormat, (GLint)texData.tex_w, (GLint)texData.tex_h, 0, glFormat, pixelType, 0);  // init to black...
 
