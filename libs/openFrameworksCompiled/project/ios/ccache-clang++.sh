73e38755b8a3c343ce0c78b356bd2606ad394a9c
Oriol Ferrer Mesia <oriol@uri.cat>
2020-02-02 10:25:17 +1100

    ccache config tweak on ios too

--- libs/openFrameworksCompiled/project/ios/ccache-clang++.sh
+++ libs/openFrameworksCompiled/project/ios/ccache-clang++.sh
@@ -4,7 +4,7 @@ if type -p /usr/local/bin/ccache >/dev/null 2>&1; then
   export CCACHE_CPP2=true
   export CCACHE_HARDLINK=true
   export CCACHE_NOHASHDIR=true
-  export CCACHE_SLOPPINESS=file_macro,time_macros,include_file_mtime,include_file_ctime,file_stat_matches
+  export CCACHE_SLOPPINESS=file_macro,time_macros,include_file_mtime,include_file_ctime,file_stat_matches,clang_index_store,locale
   export CCACHE_BASEDIR=$HOME
   exec /usr/local/bin/ccache /usr/bin/clang++ "$@"
 else
